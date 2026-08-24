3240a7d367b0936fd0b896b66155ec2c9109de44
Oluwasegun <olufasco1@gmail.com>
2026-08-24 23:42:37 +0100

    Created the project folders

---
+++ Models/Match.cs
@@ -0,0 +1,8 @@
+public class Match {
+    public int Id { get; set; }
+    public int PlayerAId { get; set; }
+    public int PlayerBId { get; set; }
+    public int ScoreA { get; set; }
+    public int ScoreB { get; set; }
+    public bool Forfeited { get; set; }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,7 @@
+public class Player {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Points { get; set; }
+    public int Goals { get; set; }
+    public int DivisionId { get; set; }
+}
